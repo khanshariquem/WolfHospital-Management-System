47e3d0ecb0de09b141deca27db63c205309d298f
vivin <vivinya25@gmail.com>
2019-04-13 00:39:01 -0400

    Init, createcheckin, update Medical Record GetPatientHistory with Util and Validation classes

--- CSC540/src/Doctor.java
+++ CSC540/src/Doctor.java
@@ -10,21 +10,9 @@ public class Doctor {
 		while (true) {
 			System.out.println("Hi "+User.name+" , Welcome to WolfHospital Management System");
 			System.out.println("Menu:");
-			System.out.println("1. Add new staff");
-			System.out.println("2. Add new Ward");
-			System.out.println("3. Add new patient");
-			System.out.println("4. Update staff details");
-			System.out.println("5. Update ward details");
-			System.out.println("6. Update pateint details");
-			System.out.println("7. Delete a staff");
-			System.out.println("8. Delete a ward");
-			System.out.println("9. Delete a pateint");
-			System.out.println("10. Check bed availability");
-			System.out.println("11. Assign a bed to patient");
-			System.out.println("12. Reserve a bed for patient");
-			System.out.println("13. Release a bed");
-			System.out.println("14. Sign Out");
-			System.out.println("15. Exit");
+			System.out.println("1. Update Medical Record");
+			System.out.println("2. Sign Out");
+			System.out.println("3. Exit");
 			System.out.print("Enter Choice : ");
 			int choice = input.nextInt();
 			switch (choice) {
@@ -32,34 +20,10 @@ public class Doctor {
 				updateMedicalRecord(input);
 				break;
 			case 2:
-				break;
-			case 3:
-				break;
-			case 4:
-				break;
-			case 5:
-				break;
-			case 6:
-				break;
-			case 7:
-				break;
-			case 8:
-				break;
-			case 9:
-				break;
-			case 10:
-				break;
-			case 11:
-				break;
-			case 12:
-				break;
-			case 13:
-				break;
-			case 14:
 				User.name = null;
 				Index.homePage(input);
 				break;
-			case 15:
+			case 3:
 				Connector.closeConnection();
 				System.out.println("Thank you for using the application! Hope to see you soon !");
 				System.exit(0);
@@ -153,22 +117,22 @@ public class Doctor {
 			System.out.println("1.Name");
 			System.out.println("2.Cost");
 			System.out.println("3.Manufacturer");
-			String choices=input.nextLine();
+			String choices=input.next();
 			String [] choice=choices.split(",");
 			HashMap<String,String> update=new HashMap<String,String>();
 			for(String c:choice){
 				switch (c) {
 					case "1":
 						System.out.println("Enter Name");
-						update.put("Name",input.nextLine());
+						update.put("Name",input.next());
 						break;
 					case "2":
 						System.out.println("Enter Cost");
-						update.put("Cost",input.nextLine());
+						update.put("Cost",input.next());
 						break;
 					case "3":
 						System.out.println("Enter Manufacturer");
-						update.put("Manufacturer",input.nextLine());
+						update.put("Manufacturer",input.next());
 						break;
 					default:
 						break;
